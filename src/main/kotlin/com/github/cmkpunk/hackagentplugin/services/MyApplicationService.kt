a5175aadcf7e065ab6ca883852b9704d84668aa6
GitHub Action <action@github.com>
2022-10-17 16:42:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/cmkpunk/hackagentplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cmkpunk/hackagentplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cmkpunk.hackagentplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cmkpunk.hackagentplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cmkpunk/hackagentplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cmkpunk/hackagentplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cmkpunk.hackagentplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cmkpunk.hackagentplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
